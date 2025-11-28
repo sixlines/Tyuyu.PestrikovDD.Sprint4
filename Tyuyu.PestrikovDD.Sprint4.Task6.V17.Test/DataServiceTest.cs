0c0eb413e5615640565ec5decf15100fbd5b0ea8
PestrikovDD <theuselessfox1@gmail.com>
2025-11-28 17:02:38 +0500

    Task6

---
+++ Tyuyu.PestrikovDD.Sprint4.Task6.V17.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuyu.PestrikovDD.Sprint4.Task6.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuyu.PestrikovDD.Sprint4.Task6.V17/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuyu.PestrikovDD.Sprint4.Task6.V17.Lib;
+DataService ds = new DataService();
+Random rnd = new Random();
+Console.Title = "Спринт #4 | Выполнил: Пестриков Д. Д. | ИСТНБ-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема:  Двумерные массивы. (генератор случайных чисел)                   *");
+Console.WriteLine("* Задание #6                                                              *");
+Console.WriteLine("* Вариант #17                                                             *");
+Console.WriteLine("* Выполнил: Пестриков Данила Дмитриевич | ИСТНБ-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан строковый массив данных используя класс Array подсчитайте количество*");
+Console.WriteLine("* элементов, длина которых равна 6                                        *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+var progr = new string[] { "Python", "JavaScript", "Java", "C#", "Swift", "Kotlin", "Ruby" };
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < progr.Length; i++)
+{
+    Console.WriteLine(progr[i]);
+}
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Количество элементов длина которых = 6: " + ds.Calculate(progr));
+Console.ReadKey();
